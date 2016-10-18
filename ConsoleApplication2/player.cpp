c6467ea4cec673f7b8a9d831553b6fd88129e1fc
Genís Bayona <genis.bayona@est.fib.upc.edu>
2016-10-18 19:54:11 +0200

    Update player.cpp
    generic aproach : )

--- ConsoleApplication2/player.cpp
+++ ConsoleApplication2/player.cpp
@@ -11,8 +11,8 @@ player::player(sf::Sprite sprite, sf::Vector2f pos, int typ)
 	
 void player::update()
 	{
-		bottom = sp.getPosition().y + 75;
-		right = sp.getPosition().x + 75;
+		bottom = sp.getPosition().y + sp.getGlobalBounds().height;
+		right = sp.getPosition().x + sp.getGlobalBounds().width;
 		left = sp.getPosition().x;
 		top = sp.getPosition().y;
 	}
